a38b04c1ed6fdc4e65f7cf9749eb1636e8a7ea17
miket-code <mikethwala496@gmail.com>
2025-09-08 02:45:04 +0200

    Update Closing.jsx

--- src/components/Closing.jsx
+++ src/components/Closing.jsx
@@ -53,7 +53,7 @@ function Closing() {
               <button
                 className="px-4 py-2 flex justify-center items-center bg-white/20 gap-2 hover:bg-white/30 backdrop-blur-sm text-white text-sm border border-white/50 rounded-lg"
                 onClick={() =>
-                  (window.location.href = "http://localhost:5173/")
+                  (window.location.href = "youtube.com")
                 }
               >
                 Next leaving this site
